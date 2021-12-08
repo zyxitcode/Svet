62937f4c1a2e4728a6ccf99053795528ed6d2836
Zyxit Code <zyxit.code@icloud.com>
2021-12-08 08:05:19 +0200

    stage 1

--- Svet/ViewController.swift
+++ Svet/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UIView!
+    @IBOutlet var yellowLabel: UIView!
+    @IBOutlet var greenLabel: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.layer.cornerRadius = 50
+        redLabel.alpha = 0.3
+        
+        yellowLabel.layer.cornerRadius = 50
+        yellowLabel.alpha = 0.3
+        
+        greenLabel.layer.cornerRadius = 50
+        greenLabel.alpha = 0.3
+    
+    
     }
 
-
+    @IBAction func startButton() {
+        
+    
+        
+        
+    }
+    
 }
 
