e6f55d024d3ae8b1a312ac1d52c467dc31ba6123
Zyxit Code <zyxit.code@icloud.com>
2021-12-08 09:00:18 +0200

    stage 2

--- Svet/ViewController.swift
+++ Svet/ViewController.swift
@@ -12,25 +12,48 @@ class ViewController: UIViewController {
     @IBOutlet var redLabel: UIView!
     @IBOutlet var yellowLabel: UIView!
     @IBOutlet var greenLabel: UIView!
+    @IBOutlet var startNextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         redLabel.layer.cornerRadius = 50
-        redLabel.alpha = 0.3
+        redLabel.alpha = 0.2
         
         yellowLabel.layer.cornerRadius = 50
-        yellowLabel.alpha = 0.3
+        yellowLabel.alpha = 0.2
         
         greenLabel.layer.cornerRadius = 50
-        greenLabel.alpha = 0.3
-    
+        greenLabel.alpha = 0.2
+
+        startNextButton.setTitleColor(UIColor.white, for: .normal)
+        
     
     }
 
     @IBAction func startButton() {
         
-    
+        if startNextButton.isTouchInside {
+            redLabel.alpha = 1
+            yellowLabel.alpha = 0.2
+            greenLabel.alpha = 0.2
+            startNextButton.setTitle("NEXT", for: .normal)
+        }
+        
+        if startNextButton.isTouchInside {
+            redLabel.alpha = 0.2
+            yellowLabel.alpha = 1
+            greenLabel.alpha = 0.2
+            startNextButton.setTitle("NEXT", for: .normal)
+        }
+        
+        if startNextButton.isTouchInside {
+            redLabel.alpha = 0.2
+            yellowLabel.alpha = 0.2
+            greenLabel.alpha = 1
+            startNextButton.setTitle("NEXT", for: .normal)
+        }
+        
         
         
     }
